89e2abbc3a26614d3c866c16444b1228de72d735
Mohammad Araf <159543785+Araf1011@users.noreply.github.com>
2025-10-27 11:12:20 +0600

    Add Bubble Sort implementation in BubbleSort.cpp
    Implement Bubble Sort algorithm to sort a vector of integers.

---
+++ BubbleSort.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    
+//  merge sort
+ int n; cin >> n;
+
+ vector<int>v(n);
+
+ for(int i=0;i<n;i++)
+ {
+     cin >> v[i];
+ }
+
+ for(int i=n-1;i>0;i--)
+ {
+     for(int j=0;j<i;j++)
+     {
+         if(v[j]>v[j+1])
+         {
+             swap(v[j],v[j+1]);
+         }
+     }
+ }
+
+ for(int i=0;i<n;i++)
+ {
+     cout << v[i] << " ";
+ }
+
+}
